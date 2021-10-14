d4e374fecde02b829f2c739cf618d028f7d38e0b
Nmuta Jones <nmuta1@gmail.com>
2021-10-14 07:18:14 -0600

    Connection

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
-// mongodb+srv:
+// mongodb+srv:`
 
 module.exports = {
-  url: "mongodb+srv://nmuta-user-one:IhVONZinZ53vru2e@cluster0.c20rr.mongodb.net/tutorials?retryWrites=true&w=majority"
+  url: "your-connection-string"
 };
  
